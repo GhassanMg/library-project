6be02c322e85db4b85dbc7ab0a561e5657632694
ghassan_mg <ghassan.mgsyr@gmail.com>
2022-12-04 16:50:27 +0300

    add cart icon with model&controller

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -106,6 +106,25 @@
                         </a>
                     </li>
                 </ul>
+                <ul class="nav navbar-nav flex-row justify-content-md-center justify-content-start flex-nowrap">
+                    <li class="nav-item  @if (request()->routeIs('about')) active @endif">
+                        <a class="nav-link" href="{{ route('about') }}">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                                <!-- Download SVG icon from http://tabler-icons.io/i/file-text -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <circle cx="6" cy="19" r="2"></circle>
+                                    <circle cx="17" cy="19" r="2"></circle>
+                                    <path d="M17 17h-11v-14h-2"></path>
+                                    <path d="M6 5l14 1l-1 7h-13"></path>
+                                 </svg>
+                            </span>
+                            <span class="nav-link-title">
+                                {{ __('Cart') }}
+                            </span>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
